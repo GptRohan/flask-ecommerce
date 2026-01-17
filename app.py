ef4197285038c3dbd2dc143271dd15d5e743bee9
ROHAN PAWAR <85385474+GptRohan@users.noreply.github.com>
2026-01-17 15:13:24 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,53 @@
+from flask import Flask, render_template, request, redirect
+from db import get_db_connection
+from db import products_collection
+
+# Server = a program that stays ON and responds when someone asks for something.
+app = Flask(__name__)
+
+from db import get_products_collection
+
+@app.route("/")
+def home():
+    products = get_products_collection().find()
+    return render_template("home.html", products=products)
+
+@app.route("/add", methods=["POST"])
+def add():
+    name = request.form["name"]
+    price = request.form["price"]
+
+    get_products_collection().insert_one({
+        "name": name,
+        "price": price
+    })
+    return redirect("/")
+
+@app.route("/delete/<id>")
+def delete(id):
+    get_products_collection().delete_one({"_id": ObjectId(id)})
+    return redirect("/")
+
+
+from bson.objectid import ObjectId
+
+@app.route("/edit/<id>", methods=["GET", "POST"])
+def edit(id):
+    col = get_products_collection()
+
+    if request.method == "POST":
+        col.update_one(
+            {"_id": ObjectId(id)},
+            {"$set": {
+                "name": request.form["name"],
+                "price": request.form["price"]
+            }}
+        )
+        return redirect("/")
+
+    product = col.find_one({"_id": ObjectId(id)})
+    return render_template("edit.html", product=product)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ db.py
@@ -0,0 +1,10 @@
+from pymongo import MongoClient
+import os
+
+MONGO_URL = os.getenv("MONGO_URL")
+
+client = MongoClient(MONGO_URL)
+db = client["ecommerce"]
+
+def get_products_collection():
+    return db["products"]
